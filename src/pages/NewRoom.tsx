6d63d9813160703f5559bcc05119abc2d4e9b24a
cicero.gabriel <cicero.gabriel@quark.tec.br>
2021-06-27 11:40:51 -0300

    Projeto finalizado

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,52 @@
+import { Link, useHistory } from 'react-router-dom';
+import { FormEvent,useState } from 'react';
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+
+import '../styles/auth.scss'
+import { Button } from '../components/Button';
+
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
+
+export function NewRoom(){
+    const {user} = useAuth();
+    const [newRoom, setNewRoom] = useState('');
+    const history = useHistory();
+    async function handleCreateRoom(event: FormEvent){
+        event.preventDefault();
+        if(newRoom.trim() === ''){
+            return;
+        }
+        const roomRef = database.ref('rooms');
+
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id
+        })
+        history.push(`/rooms/${firebaseRoom.key}`)
+    }
+    
+    return(
+        <div id="page-auth">
+        <aside>
+            <img src={illustrationImg} alt="Ilustração que simboliza perguntas e respostas" />
+            <strong>Crie salas de Q&amp;A ao-vivo</strong>
+            <p>Tire as dúvidas de sua audiência em tempo real</p>
+        </aside>
+        <main>
+            <div className="main-content">
+                <img src={logoImg} alt="Let me ask" />
+                <h1>{user?.name}</h1>
+                <h2>Criar uma nova sala</h2>
+                <form action="" onSubmit={handleCreateRoom}>
+                    <input type="text" placeholder="Digite o código da sala" onChange={event=> setNewRoom(event.target.value)} value={newRoom} />
+                    <Button type="submit">Criar sala </Button>
+                </form>
+                <p>Quer entrar em uma sala ? <Link to="/">clique aqui</Link> </p>
+            </div>
+        </main>
+        </div>
+    )
+}
